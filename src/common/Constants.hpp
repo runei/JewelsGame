cdbe52f2c21ec20fa9fc785dd16dd232e909acd1
runei <leo.invernizzi@hotmail.com>
2023-08-22 14:42:44 +0100

    improve code quality Constants

--- src/common/Constants.hpp
+++ src/common/Constants.hpp
@@ -1,37 +1,30 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-class Constants {
-public:
-	static constexpr const char* GAME_NAME = "Jewel Game";
+namespace Constants {
+
+    constexpr const char* GAME_NAME = "Jewel Game";
 
     // Configuration
-    static constexpr const char* COLOUR_CONFIG_PATH = "config/ColourConfig.json";
+    constexpr const char* COLOUR_CONFIG_PATH = "config/ColourConfig.json";
 
     // Frames
-    static constexpr int FRAME_RATE = 60;
-    static constexpr int TIME_PER_FRAME = 1000 / FRAME_RATE;
-
+    constexpr int FRAME_RATE = 60;
+    constexpr int TIME_PER_FRAME = 1000 / FRAME_RATE;
 
     // Grid dimensions
-    static constexpr int GRID_ROWS = 8;
-    static constexpr int GRID_COLS = 8;
-    static constexpr int JEWEL_SIZE = 64;
+    constexpr int GRID_ROWS = 8;
+    constexpr int GRID_COLS = 8;
+    constexpr int JEWEL_SIZE = 64;
 
     // Screen dimensions
-    static constexpr int SCREEN_WIDTH = JEWEL_SIZE * (GRID_ROWS+4);
-    static constexpr int SCREEN_HEIGHT = JEWEL_SIZE * GRID_COLS;
-
-    // Offsets for grid in the screen
-    static constexpr int OFFSET_ROW = 0;
-    static constexpr int OFFSET_COL = 0;
+    constexpr int SCREEN_WIDTH = JEWEL_SIZE * (GRID_ROWS + 4);
+    constexpr int SCREEN_HEIGHT = JEWEL_SIZE * GRID_COLS;
 
     // Game-related constants
-    static constexpr int INITIAL_SCORE = 0;
-    static constexpr int INITIAL_TIME = 60;
+    constexpr int INITIAL_SCORE = 0;
+    constexpr int INITIAL_TIME = 60;
 
-private:
-    Constants();
-};
+} // namespace GameConstants
 
 #endif // CONSTANTS_HPP
