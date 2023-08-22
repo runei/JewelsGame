b79d199ce4e914759b9d4507f33b68a2a210896e
runei <leo.invernizzi@hotmail.com>
2023-08-22 15:51:28 +0100

    improve code quality Button

--- src/components/Button.cpp
+++ src/components/Button.cpp
@@ -3,35 +3,44 @@
 
 #include "../external/loguru.hpp"
 
+// Constructor with parameters to set initial position and size
 Button::Button(SDL_Renderer** renderer, const char* buttonText, int x, int y, int width, int height)
-    : m_renderer(renderer), m_x(x), m_y(y), m_height(height), m_width(width), m_text(buttonText) {
+    : m_renderer(renderer), m_x(x), m_y(y), m_width(width), m_height(height), m_text(buttonText) {
 
     m_rect = {x, y, width, height};
 }
 
+// Constructor with only renderer and text
 Button::Button(SDL_Renderer** renderer, const char* buttonText)
     : m_renderer(renderer), m_text(buttonText) {
-
-    }
-
-Button::Button() {}
-
-
-Button::~Button() {
-    SDL_DestroyTexture(m_texture);
 }
 
+// Default constructor
+Button::Button() {
+}
+
+// Destructor
+Button::~Button() {
+}
+
+// Render the button onto the renderer
 void Button::render() {
+    // Create a texture for the button using SDLUtils
     SDL_Texture* texture = SDLUtils::createButtonTexture(*m_renderer, m_text, m_x, m_y, m_width, m_height);
 
+    // Render the texture onto the renderer
     SDL_RenderCopy(*m_renderer, texture, nullptr, &m_rect);
+
+    SDL_DestroyTexture(texture);
 }
 
+// Handle mouse events on the button
 bool Button::handleEvent(const SDL_Event& event) {
     if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
         int mouseX = event.button.x;
         int mouseY = event.button.y;
 
+        // Check if the mouse is within the button's bounds
         if (mouseX >= m_rect.x && mouseX < m_rect.x + m_rect.w &&
             mouseY >= m_rect.y && mouseY < m_rect.y + m_rect.h) {
             if (m_onclickCallback) {
@@ -43,10 +52,12 @@ bool Button::handleEvent(const SDL_Event& event) {
     return false;
 }
 
+// Set the callback function to be executed on button click
 void Button::setOnclickFunction(std::function<void()> callback) {
     m_onclickCallback = callback;
 }
 
+// Set the button's position and size
 void Button::setPositionAndSize(int x, int y, int width, int height) {
     m_x = x;
     m_y = y;
--- src/components/Button.hpp
+++ src/components/Button.hpp
@@ -3,22 +3,34 @@
 
 #include "SDL2/SDL.h"
 #include <functional>
+#include <memory>
 
 class Button {
 public:
     Button();
+
+    // Constructor with only renderer and text
     Button(SDL_Renderer** renderer, const char* buttonText);
+
+    // Constructor with parameters to set initial position and size
     Button(SDL_Renderer** renderer, const char* buttonText, int x, int y, int width, int height);
+
     ~Button();
 
+    // Render the button onto the renderer
     void render();
+
+    // Handle mouse events on the button
     bool handleEvent(const SDL_Event& event);
+
+    // Set the callback function to be executed on button click
     void setOnclickFunction(std::function<void()> callback);
+
+    // Set the button's position and size
     void setPositionAndSize(int x, int y, int width, int height);
 
 private:
     SDL_Renderer** m_renderer;
-    SDL_Texture* m_texture;
     SDL_Rect m_rect;
     std::function<void()> m_onclickCallback;
 
