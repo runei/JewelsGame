fb5d29bce6cf0144a9835ade4eb9d410073e0c6d
runei <leo.invernizzi@hotmail.com>
2023-08-22 07:45:54 +0100

    improved code quality ColourManager

--- src/common/ColourManager.cpp
+++ src/common/ColourManager.cpp
@@ -7,14 +7,13 @@
 #include <random>
 
 ColourManager::ColourManager() {
-
-	loadColoursFromConfig();
-
+    // Load colours from configuration when the manager is constructed
+    loadColoursFromConfig();
 }
 
-ColourManager::~ColourManager() {
-}
+ColourManager::~ColourManager() {}
 
+// Find the image path associated with the given colour
 std::string ColourManager::getImgPathForColour(const Colour& colour) const {
     auto it = m_colourImgMap.find(colour);
     if (it != m_colourImgMap.end()) {
@@ -24,65 +23,66 @@ std::string ColourManager::getImgPathForColour(const Colour& colour) const {
 }
 
 Colour ColourManager::getColourByName(const std::string& name) {
-
-	static std::unordered_map<std::string, Colour> nameToColourMap = {
-		{"Black", Colour::Black},
+    // Mapping of colour names to Colour enumeration values
+    static std::unordered_map<std::string, Colour> nameToColourMap = {
+        {"Black", Colour::Black},
         {"White", Colour::White},
         {"Pink", Colour::Pink},
         {"Blue", Colour::Blue},
         {"Orange", Colour::Orange}
-	};
+    };
 
-	auto it = nameToColourMap.find(name);
-	if (it != nameToColourMap.end()) {
-		return it->second;
-	}
-
-	return Colour::Unknown;
-
+    // Find the Colour based on the provided name
+    auto it = nameToColourMap.find(name);
+    if (it != nameToColourMap.end()) {
+        return it->second;
+    }
+    return Colour::Unknown;
 }
 
 Colour ColourManager::getRandomColour() const {
     const int count = static_cast<int>(Colour::Unknown);
 
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
+    // Generate a random number within the range of available colours
+    static std::mt19937 gen(std::random_device{}());
     static std::uniform_int_distribution<int> colourDistribution(0, count - 1);
 
+    // Convert the random number to a Colour enumeration value
     return static_cast<Colour>(colourDistribution(gen));
 }
 
 bool ColourManager::loadColoursFromConfig() {
+    LOG_SCOPE_FUNCTION(INFO);
+
+    LOG_F(INFO, "Loading colour configuration from: %s", Constants::COLOUR_CONFIG_PATH);
+
+    std::ifstream configFile(Constants::COLOUR_CONFIG_PATH);
+    if (!configFile.is_open()) {
+        LOG_F(ERROR, "Error opening config file: %s", Constants::COLOUR_CONFIG_PATH);
+        return false;
+    }
+
     try {
-        LOG_F(INFO, "Loading colour configuration from: %s", Constants::COLOUR_CONFIG_PATH);
-
-        // Open config file
-        std::ifstream configFile(Constants::COLOUR_CONFIG_PATH);
-        if (!configFile.is_open()) {
-            LOG_F(ERROR, "Error opening config file: %s", Constants::COLOUR_CONFIG_PATH);
-            return false;
-        }
-
-        // Create Json object
         nlohmann::json root;
         configFile >> root;
 
         LOG_F(INFO, "Parsing colour configuration...");
 
-        // Fill texture map
         const nlohmann::json& coloursObject = root["Colours"];
-		for (auto it = coloursObject.begin(); it != coloursObject.end(); ++it) {
-		    const std::string& colourName = it.key(); // The key is the colour name
-		    const std::string& imagePath = it.value().get<std::string>(); // The value is the image path
+        for (auto& entry : coloursObject.items()) {
+            const std::string& colourName = entry.key();
+            const std::string& imagePath = entry.value().get<std::string>();
 
-		    Colour colourType = getColourByName(colourName);
-		    if (colourType != Colour::Unknown) {
-		        m_colourImgMap[colourType] = imagePath;
-		        LOG_F(INFO, "Loaded colour: %s - Image Path: %s", colourName.c_str(), imagePath.c_str());
-		    } else {
-		        LOG_F(WARNING, "Unknown colour: %s, ignoring...", colourName.c_str());
-		    }
-		}
+            // Convert the colour name to a Colour enumeration value
+            Colour colourType = getColourByName(colourName);
+            if (colourType != Colour::Unknown) {
+                // Store the image path associated with the colour
+                m_colourImgMap.emplace(colourType, imagePath);
+                LOG_F(INFO, "Loaded colour: %s - Image Path: %s", colourName.c_str(), imagePath.c_str());
+            } else {
+                LOG_F(WARNING, "Unknown colour: %s, ignoring...", colourName.c_str());
+            }
+        }
 
         LOG_F(INFO, "Colour configuration loaded successfully.");
         return true;
@@ -91,4 +91,3 @@ bool ColourManager::loadColoursFromConfig() {
         return false;
     }
 }
-
--- src/common/ColourManager.hpp
+++ src/common/ColourManager.hpp
@@ -1,30 +1,34 @@
 #ifndef COLOUR_MANAGER_HPP
 #define COLOUR_MANAGER_HPP
 
-#include "../external/json.hpp"
-
 #include <unordered_map>
 #include <string>
-
+#include "../external/json.hpp"
 #include "../model/Colour.hpp"
 
+
+// Class for managing colours
 class ColourManager {
 public:
-	ColourManager();
-	~ColourManager();
+    // Constructor and Destructor
+    ColourManager();
+    ~ColourManager();
 
-	std::string getImgPathForColour(const Colour& colour) const;
-	Colour getRandomColour() const;
+    // Get the image path for a given colour
+    std::string getImgPathForColour(const Colour& colour) const;
+
+    // Get a random colour
+    Colour getRandomColour() const;
 
 private:
+    // Mapping of colours to image paths
+    std::unordered_map<Colour, std::string> m_colourImgMap;
 
-	std::unordered_map<Colour, std::string> m_colourImgMap;
+    // Get a Colour based on its name
+    Colour getColourByName(const std::string& name);
 
-	Colour getColourByName(const std::string& name);
-
-	bool loadColoursFromConfig();
-
-
+    // Load colours from a configuration
+    bool loadColoursFromConfig();
 };
 
 #endif // COLOUR_MANAGER_HPP
